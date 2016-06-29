42abc7a96ee586fd87426b31ff77b056e4a47af1
Joe <joew@fnvi.co.uk>
2016-06-29 16:59:02 +0100

    Random test added to prove unit testing is correctly configured

---
+++ tests/AuthTest.php
@@ -0,0 +1,16 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use FNVi\Authentication;
+
+/**
+ * Description of AuthTests
+ *
+ * @author Joe Wheatley <joew@fnvi.co.uk>
+ */
+class AuthTests extends TestCase{
+    
+    public function testWorking(){
+        $this->assertEquals(5,5);
+    }
+    
+}
