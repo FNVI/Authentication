a09e003cb1dc2a8851bf028d1eb65784ad55a169
Joe <joew@fnvi.co.uk>
2016-07-20 15:43:27 +0100

    Added url fixes

--- examples/regular/settings.php
+++ examples/regular/settings.php
@@ -7,3 +7,6 @@ define("REDIRECT_FORBIDDEN", "forbidden.html");
 define("REDIRECT_UNAUTHORIZED", "unauthorised.html");
 
 define("EMAIL_ADDRESS","info@fnvi.co.uk");
+
+define("URL_CONFIRM_EMAIL", "joe/authentication/examples/regular/confirmemail.php?");
+define("URL_FORGOTTEN_PASSWORD","joe/authentication/examples/regular/resetpassword.php?");
