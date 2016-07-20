63da6b013260dd3ed632d593be56e1d0e0db22b4
Joe <joew@fnvi.co.uk>
2016-07-20 15:41:18 +0100

    Updated tests

---
+++ tests/SessionTest.php
@@ -0,0 +1,53 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use FNVi\Authentication\Schemas\User;
+use FNVi\Authentication\Schemas\Session;
+
+/**
+ * Description of SessionTest
+ *
+ * @author Joe Wheatley <joew@fnvi.co.uk>
+ */
+class SessionTest extends TestCase{
+
+    /**
+     * 
+     * @return Session
+     */
+    public function testConstructor(){
+        
+        $this->assertFalse(Session::keyExists());
+        $session = new Session();
+        $this->assertTrue(Session::keyExists());
+        return $session;
+    }
+    
+    /**
+     * @depends testConstructor
+     * @param Session $session
+     * @return Session
+     */
+    public function testSetUser(Session $session){
+        
+        $user = new User("user","password");
+        $user->store();
+//        $session->setUser($user);
+        $session->store();
+        return $session;
+    }
+    
+//    /**
+//     * @depends testGetUser
+//     * @param Session $session
+//     * @return Session
+//     */
+//    public function testGetUser(Session $session){
+//        
+//        $user = $session->getUser();
+//        
+//        $this->assertNotNull($user);
+//        $this->assertEquals("user", $user->username);
+//        return $session;
+//    }
+    
+}
