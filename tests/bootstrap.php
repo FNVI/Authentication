6665444b121aebdcccca594624f9a2feb8eb0341
Joe <joew@fnvi.co.uk>
2016-07-28 16:15:46 +0100

    Added global constants

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,15 @@
 
 define("DATABASE", "testdb");
 define("MONGOURI","mongodb://localhost");
+
+define("REDIRECT_FORBIDDEN", "forbidden.html");
+define("REDIRECT_UNAUTHORIZED", "unauthorised.html");
+
+define("EMAIL_ADDRESS","info@fnvi.co.uk");
+
+define("URL_CONFIRM_EMAIL", "joe/authentication/examples/regular/confirmemail.php?");
+define("URL_FORGOTTEN_PASSWORD","joe/authentication/examples/regular/resetpassword.php?");
+
 include 'vendor/autoload.php';
 
 // ob_start required to allow session headers to be set!
