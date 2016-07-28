2c5e407b887182818c7d22bd72e271395be2fb44
Joe <joew@fnvi.co.uk>
2016-07-28 16:16:40 +0100

    Added tests for main auth class

---
+++ tests/AuthTest.php
@@ -0,0 +1,55 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use FNVi\Authentication\Auth;
+use FNVi\Authentication\Schemas\Session;
+use FNVi\Authentication\Schemas\User;
+use FNVi\Mongo\Database;
+use MongoDB\BSON\ObjectID;
+/**
+ * Description of AuthTest
+ *
+ * @author Joe Wheatley <joew@fnvi.co.uk>
+ */
+class AuthTest extends TestCase{
+    
+    protected $user;
+    
+    protected function setUp() {
+        $this->user = new User("user","password");
+        $this->user->email = "joew@fnvi.co.uk";
+        parent::setUp();
+    }
+    
+    
+
+
+    public function testConstructor(){
+        $auth = new Auth();
+        return $auth;
+    }
+    
+    /**
+     * @depends testConstructor
+     * @param Auth $auth
+     * @return Auth
+     */
+    public function testSignup(Auth $auth){
+        $token = $auth->registerUser($this->user,true);
+        $this->assertNotFalse($token);
+        $result = $auth->confirmEmail($this->user, $token);
+        $this->assertTrue($result, $auth->getMessage());
+        return $auth;
+    }
+    
+    public function testTokens(){
+        $id = new ObjectID();
+        $token = Auth::issueToken($id);
+        $actual = Auth::checkToken($id, $token);
+        $this->assertTrue($actual);
+    }
+        
+    public static function tearDownAfterClass() {
+        Session::stop();
+        parent::tearDownAfterClass();
+    }
+}
