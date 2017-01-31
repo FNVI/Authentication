6d70c4d9908367a17370f06c8dd218cc4b0f88d8
Joe <joew@fnvi.co.uk>
2017-01-31 13:52:36 +0000

    Tidied unit test bootstrap file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,17 +1,11 @@
 <?php
-
-define("DATABASE", "testdb");
-define("MONGOURI","mongodb://localhost");
-
-define("REDIRECT_FORBIDDEN", "forbidden.html");
-define("REDIRECT_UNAUTHORIZED", "unauthorised.html");
-
-define("EMAIL_ADDRESS","info@fnvi.co.uk");
-
-define("URL_CONFIRM_EMAIL", "joe/authentication/examples/regular/confirmemail.php?");
-define("URL_FORGOTTEN_PASSWORD","joe/authentication/examples/regular/resetpassword.php?");
+use FNVi\Mongo\Database;
+use FNVi\Authentication\Auth;
 
 include 'vendor/autoload.php';
 
 // ob_start required to allow session headers to be set!
 ob_start();
+
+Database::connect("mongodb://localhost/testdb");
+Auth::setMaxLoginAttempts(3);
