72497c33515ed045acd2d40e3f1938e526924ae0
Joe <joew@fnvi.co.uk>
2017-02-03 17:00:21 +0000

    Users Names collection

--- src/Collections/Users.php
+++ src/Collections/Users.php
@@ -1,21 +1,16 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace FNVi\Authentication\Collections;
 
 use FNVi\Mongo\Collection;
-use FNVi\Authentication\Schemas\User;
-use FNVi\Email;
 /**
  * Description of Users
  *
  * @author Joe Wheatley <joew@fnvi.co.uk>
  */
 class Users extends Collection{
+    
+    public function __construct() {
+        parent::__construct("users");
+    }
     
 }
